0b88e65e6618bff6d8959e093c6f40c40c30c4c0
Arunguru <arunguru00@gmail.com>
2025-06-03 22:33:01 +0530

    hdj

---
+++ conscripts.js
@@ -0,0 +1,20 @@
+function sendmain(){
+    let send_details={
+        from:document.getElementById('fname').value,
+        email:document.getElementById('email').value,
+        subject:document.getElementById('subject').value,
+        comment:document.getElementById('comment').value
+    }
+    emailjs.send('service_kbder2m','template_mm2pl8a',send_details).then(but_property()).catch((error)=>{alert('error')},);
+}
+
+function but_property(){
+    
+    document.getElementById('send').style.display='none';
+    document.getElementById('disSent').style.display='block';
+}
+
+function hello(){
+    alert('hello');
+}
+
